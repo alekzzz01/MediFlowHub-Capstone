52be925218cc8629fd64f4357c6f9873f23dae39
Alex <fajartin.alex@gmail.com>
2023-12-29 17:25:29 +0800

    alex-update

--- users/viewappointment.php
+++ users/viewappointment.php
@@ -3,7 +3,6 @@ require '../session/db.php';
 require_once '../session/session_manager.php';
 require_once '../TCPDF-main/tcpdf.php'; // Include the TCPDF library
 
-require_once('../TCPDF-main/tcpdf_include.php');
 
 
 if (!isset($_GET['appointment_id'])) {
@@ -13,8 +12,14 @@ if (!isset($_GET['appointment_id'])) {
 
 $appointmentId = $_GET['appointment_id'];
 
-// Fetch appointment details from the database based on the provided ID
-$query = "SELECT * FROM appointments WHERE Appointment_ID = $appointmentId";
+$query = "SELECT a.Appointment_ID, 
+                 p.Last_Name AS Patient_Last_Name, p.First_Name AS Patient_First_Name, 
+                 d.Last_Name AS Doctor_Last_Name, d.First_Name AS Doctor_First_Name, 
+                 a.Status, a.time_slot, a.Date
+          FROM appointments a
+          JOIN patients_table p ON a.Patient_id = p.Patient_id
+          JOIN doctors_table d ON a.doctor_id = d.doctor_id
+          WHERE a.Appointment_ID = $appointmentId";
 $result = $conn->query($query);
 
 if ($result === false || $result->num_rows === 0) {
@@ -27,22 +32,41 @@ $appointmentDetails = $result->fetch_assoc();
 
 
 
+
+
+
 // Create a PDF document
 $pdf = new TCPDF();
+
+
+
+
+
 $pdf->AddPage();
+
+$pdf->SetFont('helvetica', 'B', 16); // 'B' stands for bold
+$pdf->Cell(0, 10, 'Barangay Name', 0, 1, 'C');
+$pdf->Ln(5);
 
 // Output appointment details in the PDF
 $pdf->SetFont('helvetica', '', 12);
 $pdf->Cell(0, 10, 'Appointment Details', 0, 1, 'C');
 $pdf->Ln(10);
 $pdf->Cell(0, 10, 'Appointment ID: ' . $appointmentDetails['Appointment_ID'], 0, 1);
-$pdf->Cell(0, 10, 'Patient: ' . $appointmentDetails['Patient_id'] . ', ' , 0, 1);
-$pdf->Cell(0, 10, 'Diagnosis: ' . $appointmentDetails['Diagnosis'] . ', ' , 0, 1);
-// Add more details as needed
+$pdf->Cell(0, 10, 'Patient: ' . $appointmentDetails['Patient_Last_Name'] . ', ' . $appointmentDetails['Patient_First_Name']  , 0, 1);
+$pdf->Cell(0, 10, 'Doctor: ' . $appointmentDetails['Doctor_Last_Name'] . ', '  . $appointmentDetails['Doctor_First_Name']  , 0, 1);
+$pdf->Cell(0, 10, 'Status: ' . $appointmentDetails['Status'] , 0, 1);
+$pdf->Cell(0, 10, 'Date and Time: ' . date('F j, Y', strtotime($appointmentDetails['Date'])) . ', ' . $appointmentDetails['time_slot'], 0, 1);
 
-// Output the PDF to the browser
-$pdf->Output('appointment_details.pdf', 'I');
+
+
+
+
+
+$fileName = 'appointment_' . $appointmentDetails['Appointment_ID'] . '_' . date('F j, Y') . '.pdf';
+$pdf->Output($fileName, 'I');
 
 
 
 ?>
+
