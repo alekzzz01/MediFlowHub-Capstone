137ac60ab33e9cec6d086fd134cba5fca2da5113
Alex <fajartin.alex@gmail.com>
2024-01-08 09:44:52 +0800

    alex-update

--- users/viewappointment.php
+++ users/viewappointment.php
@@ -15,7 +15,7 @@ $appointmentId = $_GET['appointment_id'];
 
 $query = "SELECT a.Appointment_ID, 
                  p.Last_Name AS Patient_Last_Name, p.First_Name AS Patient_First_Name, 
-                 d.Last_Name AS Doctor_Last_Name, d.First_Name AS Doctor_First_Name, 
+                 d.Last_Name AS Doctor_Last_Name, d.First_Name AS Doctor_First_Name, d.Fee,
                  a.Status, a.time_slot, a.Date, a.Prescription
           FROM appointments a
           JOIN patients_table p ON a.Patient_id = p.Patient_id
@@ -121,6 +121,7 @@ $pdf->Cell(0, 10, 'Appointment ID: ' . $appointmentDetails['Appointment_ID'], 0,
 $pdf->Cell(0, 10, 'Patient: ' . $appointmentDetails['Patient_Last_Name'] . ', ' . $appointmentDetails['Patient_First_Name'], 0, 1);
 $pdf->Cell(0, 10, 'Doctor: ' . $appointmentDetails['Doctor_Last_Name'] . ', ' . $appointmentDetails['Doctor_First_Name'], 0, 1);
 $pdf->Cell(0, 10, 'Status: ' . $appointmentDetails['Status'], 0, 1);
+$pdf->Cell(0, 10, 'Fee: ' . $appointmentDetails['Fee'], 0, 1);
 $pdf->Cell(0, 10, 'Date and Time: ' . date('F j, Y', strtotime($appointmentDetails['Date'])) . ', ' . $appointmentDetails['time_slot'], 0, 1);
 $pdf->Ln(10);
 $pdf->MultiCell(0, 10, 'Prescription: ' . $appointmentDetails['Prescription'], 0, 'L');
